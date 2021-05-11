8e70a11fc79ce7ea7ee684259d705355cc94223c
linzeqin <810390198@qq.com>
2021-05-11 23:34:42 +0800

    init project

--- scripts_/plugins/index.js
+++ scripts_/plugins/index.js
@@ -10,13 +10,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.plugins = void 0;
 var clean_webpack_plugin_1 = require("clean-webpack-plugin");
-var webpack_1 = require("webpack");
 var webpackbar_1 = __importDefault(require("webpackbar"));
 var htmlPlugin_1 = require("./htmlPlugin");
 exports.plugins = __spreadArray(__spreadArray([], htmlPlugin_1.htmlPlugin), [
     new clean_webpack_plugin_1.CleanWebpackPlugin({}),
     new webpackbar_1.default({}),
-    new webpack_1.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('dev'),
-    })
 ]);
